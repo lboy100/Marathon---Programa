14e159cadfcaebd69a8487748e3ae89675716023
lboy100 <lyonard.maduro@gmail.com>
2015-03-20 15:46:29 +0100

    Marathon

--- Marathon/Form1.cs
+++ Marathon/Form1.cs
@@ -16,5 +16,10 @@ namespace Marathon
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Patat";
+        }
     }
 }
